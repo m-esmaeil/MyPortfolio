14086f1d327f4ae36770554aaab7b4d7db2aacf0
muhamed esmaeil <m_a_esmaeil@hotmail.com>
2021-05-30 23:49:44 +0200

    Add project files.

---
+++ Core/Entities/Portfolio.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Entities
+{
+    public class Portfolio : EntityBase
+    {
+        public string ProjectName { get; set; }
+        public string Description { get; set; }
+        public string ImageURL { get; set; }
+    }
+}
---
+++ Infrastructure/DataContext.cs
@@ -0,0 +1,39 @@
+﻿using Core;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Owner>().Property(x => x.Id).HasDefaultValueSql("NEWID()");
+            modelBuilder.Entity<PortfolioItem>().Property(x => x.Id).HasDefaultValueSql("NEWID()");
+
+            modelBuilder.Entity<Owner>().HasData(
+                new Owner
+                {
+                    Id=Guid.NewGuid(),
+                    FllName="Mohamed Esmaeil",
+                    Avatar="avatar.jpg",
+                    Profile="Microsoft MVP / .Net Consultant"
+                }
+                );
+        }
+
+        public DbSet<Owner> Owner { get; set; }
+        public DbSet<PortfolioItem> PortfolioItem { get; set; }
+    }
+}
